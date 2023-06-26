513258ad60bbd2061139d27070e6f7cf58723257
Artem Kurilik <artem.kurilik@gmail.com>
2023-06-26 12:58:37 +0300

    Save ex

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { SomeText } from '@components/some-text'
+import './App.css'
+
+function App() {
+  return (
+    <SomeText />
+  )
+}
+
+export default App
---
+++ src/components/some-text.tsx
@@ -0,0 +1,5 @@
+export const SomeText = () => {
+  return (
+    <div>SomeText (host app)</div>
+  )
+}
